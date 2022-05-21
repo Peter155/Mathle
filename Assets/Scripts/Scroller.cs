b52b7ee7e87d82f5e61153ebc6b1f00a354dbe3d
Ben Tang <sjps30503@gmail.com>
2022-05-21 17:13:23 -0400

    Imported font and images

---
+++ Assets/Scripts/Scroller.cs
@@ -0,0 +1,14 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Scroller : MonoBehaviour
+{
+    [SerializeField] private RawImage _image;
+    [SerializeField] private float _x, _y;
+
+    private void Update()
+    {
+        _image.uvRect = new Rect(_image.uvRect.position + new Vector2(_x, _y) * Time.deltaTime, _image.uvRect.size);
+    }
+}
