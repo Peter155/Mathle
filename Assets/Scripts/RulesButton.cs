f27890f333bd618bdfdc7e7452dd59361ce7168b
Peter155 <pgw25@drexel.edu>
2022-06-01 13:43:32 -0400

    Added Sounds
    Added sounds to buttons and win or lose, also Hunter can't cheat anymore

--- Assets/Scripts/RulesButton.cs
+++ Assets/Scripts/RulesButton.cs
@@ -5,12 +5,18 @@ using UnityEngine;
 public class RulesButton : MonoBehaviour
 {
     public GameObject rulesScreen;
+
+    public AudioSource open;
+    public AudioSource close;
+
     public void ToggleRules()
     {
         if(rulesScreen.activeSelf) {
             rulesScreen.SetActive(false);
+            close.Play();
         } else {
             rulesScreen.SetActive(true);
+            open.Play();
         }
     }
 }
